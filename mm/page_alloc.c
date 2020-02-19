6946bd25398be0648c7794c914daaa414ae45209
Sultan Alsawaf <sultan@kerneltoast.com>
2020-02-19 14:47:13 -0800

    mm: Stop kswapd early when nothing's waiting for it to free pages
    Keeping kswapd running when all the failed allocations that invoked it
    are satisfied incurs a high overhead due to unnecessary page eviction
    and writeback, as well as spurious VM pressure events to various
    registered shrinkers. When kswapd doesn't need to work to make an
    allocation succeed anymore, stop it prematurely to save resources.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4244,6 +4244,8 @@ __alloc_pages_slowpath(gfp_t gfp_mask, unsigned int order,
 	unsigned int cpuset_mems_cookie;
 	unsigned int zonelist_iter_cookie;
 	int reserve_flags;
+	pg_data_t *pgdat = ac->preferred_zoneref->zone->zone_pgdat;
+	bool woke_kswapd = false;
 #ifdef OPLUS_FEATURE_HEALTHINFO
 /* Huacai.Zhou@PSW.BSP.Kernel.MM, 2018-07-07, add alloc wait monitor support*/
 #ifdef CONFIG_OPPO_MEM_MONITOR
@@ -4284,8 +4286,13 @@ restart:
 	if (!ac->preferred_zoneref->zone)
 		goto nopage;
 
-	if (gfp_mask & __GFP_KSWAPD_RECLAIM)
+	if (gfp_mask & __GFP_KSWAPD_RECLAIM) {
+		if (!woke_kswapd) {
+			atomic_inc(&pgdat->kswapd_waiters);
+			woke_kswapd = true;
+		}
 		wake_all_kswapds(order, ac);
+	}
 
 	/*
 	 * The adjusted alloc_flags might result in immediate success, so try
@@ -4491,9 +4498,14 @@ nopage:
 		goto retry;
 	}
 fail:
+got_pg:
+	if (woke_kswapd)
+		atomic_dec(&pgdat->kswapd_waiters);
+	if (!page)
+		warn_alloc(gfp_mask, ac->nodemask,
+				"page allocation failure: order:%u", order);
 	warn_alloc(gfp_mask, ac->nodemask,
 			"page allocation failure: order:%u", order);
-got_pg:
 #ifdef OPLUS_FEATURE_HEALTHINFO
 /* Huacai.Zhou@PSW.BSP.Kernel.MM, 2018-07-07, add alloc wait monitor support*/
 #ifdef CONFIG_OPPO_MEM_MONITOR
@@ -6541,6 +6553,7 @@ static void __paginginit free_area_init_core(struct pglist_data *pgdat)
 	pgdat_page_ext_init(pgdat);
 	spin_lock_init(&pgdat->lru_lock);
 	lruvec_init(node_lruvec(pgdat));
+	pgdat->kswapd_waiters = (atomic_t)ATOMIC_INIT(0);
 
 	pgdat->per_cpu_nodestats = &boot_nodestats;
 
